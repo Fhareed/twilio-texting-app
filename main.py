dae25656554eac902f12d54c2acd5da5324f33a5
OladegaFarid <72567937+Fhareed@users.noreply.github.com>
2021-07-24 14:34:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACce1a293ed501bd4e2d6c66d871ae420e'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+15017122661',
+                     to='+15558675310'
+                 )
+
+print(message.sid)
+
+
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
